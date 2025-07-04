775288c8d9f2e772379fb656264977df1ce06c7a
Shifra Destiny Ssebuliba <destinyssebuliba@gmail.com>
2025-07-04 16:38:50 +0200

    Added a display function to display contact information

--- src/index.js
+++ src/index.js
@@ -8,3 +8,28 @@ function fetchContacts() {
     });
 }
 
+function displayOutput(data) {
+  let output = "<table>";
+
+  for (a in data) {
+    output += `
+      <tr>
+        <td>
+          <img
+            src="${rootPath}controller/uploads/${data[a].avatar}"
+            width="40"
+          />
+        </td>
+        <td>
+          <h5>${data[a].firstname}</h5>
+        </td>
+        <td>
+          <h5>${data[a].lastname}</h5>
+        </td>
+      </tr>
+    `;
+  }
+
+  output += "</table>";
+  document.getElementById("table").innerHTML = output;
+}
